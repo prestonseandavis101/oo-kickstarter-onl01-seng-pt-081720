779d5f7af73588aeca5b39f6adb4e427f4052364
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-19 17:47:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,13 +2,10 @@ require 'pry'
 
 class Backer
   attr_accessor :name, :backed_projects
-  #attr_reader :backer --> may not need this
   
   def initialize(name)
-    #@backer=backer --> may not need this
     @name=name
     @backed_projects = []
-    #binding.pry
   end
   
 end 
